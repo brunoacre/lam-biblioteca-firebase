96a77c73f2201276cbf0c01f418862ad3358a4f5
Bruno Couto Rezende <bruno.rezende.bd@gmail.com>
2025-10-15 12:07:12 -0300

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,12 +4,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD-ze8S1FyrqulSIw9P-LClZpIiqCWszIY",
-  authDomain: "app-react-native-2025.firebaseapp.com",
-  projectId: "app-react-native-2025",
-  storageBucket: "app-react-native-2025.appspot.com",
-  messagingSenderId: "1086621941454",
-  appId: "1:1086621941454:web:6a02516733806a2fb09a9f",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 // Initialize Firebase
